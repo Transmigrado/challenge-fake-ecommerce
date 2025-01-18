8b3477d4f1df85368895f548d3ea4c6c598e16ef
Jorge Acosta <transmigrado@gmail.com>
2025-01-18 14:03:59 -0300

    feat: add product model

---
+++ app/src/main/java/com/blueprint/fakeecommerce/model/Product.kt
@@ -0,0 +1,11 @@
+package com.blueprint.fakeecommerce.model
+
+data class Product(
+    var id: Int,
+    var title: String,
+    var price: Float,
+    var description: String,
+    var category: String,
+    var image: String
+)
+
